7d949344a8f9a5d0eb5177aac5c1e41f5c73cc36
Javi <fjmateos92@gmail.com>
2018-02-16 15:24:42 +0100

    Added webpack, lint and more

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.css'
+var numeral = require('numeral')
+
+const courseValue = numeral(1000).format('$0,0.00')
+console.log(courseValue)
+console.log('AEOEO!!!!!!!!!!!!')
---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+var path = require('path')
+
+exports.default = {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/},
+      {test: /\.css$/, loaders: ['style-loader', 'css-loader']}
+    ]
+  }
+}
